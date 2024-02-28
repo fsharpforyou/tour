9390175549eb4095c9e03346903b7b3d82122f7b
Christian Sheridan <christiansheridan@outlook.com>
2024-02-28 11:50:15 -0600

    Update fable-metadata and fable-standalone (#17)

--- src/App/Constants.fs
+++ src/App/Constants.fs
@@ -9,6 +9,6 @@ let host =
 
 let worker = host + "js/repl/worker.min.js"
 let metadata = host + "metadata"
-let fableLibrary = host + "js/repl/fable-library"
+let fableLibrary = host + "js/repl/fable-library-js"
 let documentation = host + "documentation/"
 let tableOfContents = documentation + "table-of-contents.json"
