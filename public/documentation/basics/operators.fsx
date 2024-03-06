a2cf532f152c9f22808a8d4f14ecce716686c87e
sheridanchris <christiansheridan@outlook.com>
2024-03-06 11:59:22 -0600

    Add operators page

---
+++ public/documentation/basics/operators.fsx
@@ -0,0 +1,17 @@
+let ten = 5 + 5
+printfn "5 + 5 = %d" ten
+
+let four = 8 - 4
+printfn "8 - 4 = %d" four
+
+let five = 10 / 2
+printfn "10 / 2 = %d" five
+
+let twenty = 10 * 2
+printfn "10 * 2 = %d" twenty
+
+let remainder = 10 % 2
+printfn "10 %% 2 = %d" remainder
+
+let squared = 10.0 ** 2.0
+printfn "10.0 ** 2.0 = %f" squared
